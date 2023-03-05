59ae7deab9e49e8dad632eeae6bccd251d666102
Taras <112703291+Shadow-SML@users.noreply.github.com>
2023-03-05 13:57:34 +0100

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let intervalId = null;
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function startColorSwitcher() {
+  startBtn.disabled = true;
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function stopColorSwitcher() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+
+startBtn.addEventListener('click', startColorSwitcher);
+stopBtn.addEventListener('click', stopColorSwitcher);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
